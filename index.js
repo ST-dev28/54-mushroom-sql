5cf183cba0a8fd4b3d26403a01481625d185d868
SandraT <sndrbltr@gmail.com>
2021-09-04 11:07:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+
+const app = {}
+
+app.init = async () => {
+    // prisijungti prie duomenu bazes
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'mushroom',
+    });
+
+    let sql = '';
+    let rows = [];
+
+    // LOGIC BELOW
+
+}
+
+app.init();
+
+module.exports = app;
