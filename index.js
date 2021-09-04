4d08112f3f2377aabb4146d36fa51e509b0c2c00
SandraT <sndrbltr@gmail.com>
2021-09-04 12:06:12 +0000

    Need to have by kilo each solution

--- index.js
+++ index.js
@@ -49,7 +49,33 @@ app.init = async () => {
     console.log(rows);
     console.log(`Brangiausias grybas yra: ${rows[0].mushroom}.`);
 
+    //**4.** _Isspausdinti, pigiausio grybo pavadinima
+    sql = 'SELECT MIN(price) AS SmallestPrice, `mushroom` FROM `mushroom`';
+    [rows] = await connection.execute(sql);
+    console.log(rows);
+    //console.log(`Pigiausias grybas yra: ${rows[0].mushroom}.`);
 
+    //** 5. ** _Isspausdinti, visu kiek vidutiniskai reikia grybu, 
+    //jog jie svertu 1 kilograma(suapvalinti iki vieno skaiciaus po kablelio), 
+    //isrikiuojant pagal pavadinima nuo abeceles pradzios link pabaigos
+    sql = 'SELECT `mushroom`, `weight` FROM `mushroom`';
+    [rows] = await connection.execute(sql);
+    //console.log(rows);
+    console.log(`Grybai: `);
+    let missingTillKg = 0;
+    const needKg = 1000;
+    let numb = 0;
+    for (let i = 0; i < rows.length; i++) {
+        ++numb;
+        const mushroomName = rows[i].mushroom;
+        const weightPerSort = rows[i].weight;
+        missingTillKg = needKg / weightPerSort;
+
+        const need = missingTillKg % 1 ? missingTillKg.toFixed(1) : missingTillKg.toFixed();
+        const mushroomNameFirstCapital = mushroomName.charAt(0).toUpperCase() + mushroomName.slice(1);
+
+        console.log(`${numb}) ${mushroomNameFirstCapital} - ${need}`);
+    }
 }
 
 app.init();
